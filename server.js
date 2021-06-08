0c40d8648769019b405d1a85002ad7b10705d749
bmuday <bmuday@live.fr>
2021-06-08 20:26:30 +0200

    remove cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const dotenv = require("dotenv").config();
-const cors = require("cors");
+/* const cors = require("cors"); */
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger("combined"));
 
 // CORS
-app.use(cors());
+/* app.use(cors()); */
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
