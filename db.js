0083451599e3df85b4df77ba31879663703b0f6c
bmuday <bmuday@live.fr>
2021-06-08 20:11:17 +0200

    change env vars

--- db.js
+++ db.js
@@ -4,18 +4,11 @@ const dotenv = require("dotenv");
 const { username, password, database } = process.env;
 
 // DB connection
-if (process.env.NODE_ENV === "production") {
-  const MONGODB_URI = `mongodb+srv://${username}:${password}@cluster0.nnbxv.mongodb.net/${database}?retryWrites=true&w=majority`;
-  mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-} else {
-  mongoose.connect(`mongodb://localhost/${database}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-}
+const MONGODB_URI = `mongodb+srv://${username}:${password}@cluster0.nnbxv.mongodb.net/${database}?retryWrites=true&w=majority`;
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 mongoose.connection.on("connected", () => {
   console.log("Mongoose is connected!!!");
