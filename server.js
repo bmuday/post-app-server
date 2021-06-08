7d0a9f6763d655107e21f899baceb6b81f5c90e4
bmuday <bmuday@live.fr>
2021-06-08 20:31:10 +0200

    fix decouple

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const dotenv = require("dotenv").config();
-/* const cors = require("cors"); */
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,9 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger("combined"));
 
 // CORS
-/* app.use(cors()); */
+app.use(cors());
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
   next();
-});
+}); */
 
 // For deployment
 // process.env.PORT
