4507242ccc7c11c317fcf106e820320fd51f960c
bmuday <bmuday@live.fr>
2021-06-09 00:12:05 +0200

    change cors

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const logger = require("morgan");
 const dotenv = require("dotenv").config();
-const cors = require("cors");
+/* const cors = require("cors"); */
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // CORS
-app.use(cors());
-/* app.use((req, res, next) => {
+/* app.use(cors()); */
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -19,7 +19,7 @@ app.use(cors());
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
   next();
-}); */
+});
 
 // Database Connection
 const db = require("./db");
